0e332b3e08c04b1b9a494a2616526e22ddfd4a24
Ankit.Shrivastava <Ankit.Shrivastava@IN-QA10.resonate.co.uk>
2018-05-19 19:58:43 +0530

    test it

---
+++ SeleniumAutomation/src/testcases/TC_003New.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+//*****This class is only made for testing this is not used in this project purposely *******///
+public class TC_003New {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HI this is new code");
+	}
+	
+	
+	
+	
+	
+}
